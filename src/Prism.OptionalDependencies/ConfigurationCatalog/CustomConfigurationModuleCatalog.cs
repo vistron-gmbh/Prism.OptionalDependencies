750e947dfc9ba00f27b85f73adbd20fa3a1d90aa
Manuel Blattner <m.blattner@vistron.de>
2023-05-08 11:47:47 +0200

    The custom catalog now respects dependency attributes besides the ones in the config

--- src/Prism.OptionalDependencies/ConfigurationCatalog/CustomConfigurationModuleCatalog.cs
+++ src/Prism.OptionalDependencies/ConfigurationCatalog/CustomConfigurationModuleCatalog.cs
@@ -3,20 +3,39 @@ using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Prism.Modularity.OptionalDependencies
 {
-    /// <summary>
-    /// Supports Optional tags in the module dependencies like so: ModuleName[optional]
-    /// </summary>
-    public class CustomConfigurationModuleCatalog : ConfigurationModuleCatalog
-    {
-        protected override void InnerLoad()
-        {
-            base.InnerLoad();
-            OptionalDependencyHelper.HandleOptionalDependencies(Modules); //Replaces the optional dependencies with mandatory ones if the needed module is available.
-        }
-    }
+	/// <summary>
+	/// Supports Optional tags in the module dependencies like so: ModuleName[optional]
+	/// This Configuration based catalog also handles attribute declared dependencies correctly.
+	/// </summary>
+	public class CustomConfigurationModuleCatalog : ConfigurationModuleCatalog
+	{
+		protected override void InnerLoad()
+		{
+			base.InnerLoad();
+			AddAttributeDependencies(Modules); //With this the catalog supports also dependencies declared in the type itself
+			OptionalDependencyHelper.HandleOptionalDependencies(Modules); //Replaces the optional dependencies with mandatory ones if the needed module is available.
+		}
+
+		private void AddAttributeDependencies(IEnumerable<IModuleInfo> modules)
+		{
+			foreach (IModuleInfo module in modules)
+				AddAttributeDependencies(module);
+		}
+
+		private static void AddAttributeDependencies(IModuleInfo module)
+		{
+			var type = Type.GetType(module.ModuleType);
+			var moduleDependencyAttributes =
+				CustomAttributeData.GetCustomAttributes(type).Where(
+				cad => cad.Constructor.DeclaringType.FullName == typeof(ModuleDependencyAttribute).FullName);
+			foreach (CustomAttributeData cad in moduleDependencyAttributes)
+				module.DependsOn.Add((string)cad.ConstructorArguments[0].Value);
+		}
+	}
 }
--- src/Prism.OptionalDependencies/OptionalDependencyHelper.cs
+++ src/Prism.OptionalDependencies/OptionalDependencyHelper.cs
@@ -2,55 +2,58 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace Prism.Modularity.OptionalDependencies
 {
-    /// <summary>
-    /// Identifies the [optional] tag inside strings which are used as module dependencies and will either remove the dependency if the needed module is not available or keep it with the optional tag removed.
-    /// </summary>
-    public static class OptionalDependencyHelper
-    {
-        private static Regex _OPTIONAL_TAG_REGEX = new Regex("(.*?)(\\[optional\\])");  //Captures the name itself and a possible optional tag in the form of [optional]
+	/// <summary>
+	/// Identifies the [optional] tag inside strings which are used as module dependencies and will either remove the dependency if the needed module is not available or keep it with the optional tag removed.
+	/// </summary>
+	public static class OptionalDependencyHelper
+	{
+		private static Regex _OPTIONAL_TAG_REGEX = new Regex("(.*?)(\\[optional\\])");  //Captures the name itself and a possible optional tag in the form of [optional]
 
-        /// <summary>
-        /// Checks if the optional tag is set and will remove it from the dependency if found.
-        /// </summary>
-        /// <param name="dependencyName"></param>
-        /// <returns></returns>
-        public static bool IsOptional(ref string dependencyName)
-        {
-            var match = _OPTIONAL_TAG_REGEX.Match(dependencyName);  //Captures the name itself and a possible optional tag in the form of [optional]
-            if (match.Success)
-            {
-                dependencyName = match.Groups[1].Value;
-                return true;
-            }
-            else
-                return false;
-        }
+		/// <summary>
+		/// Checks if the optional tag is set and will remove it from the dependency if found.
+		/// </summary>
+		/// <param name="dependencyName"></param>
+		/// <returns></returns>
+		public static bool IsOptional(ref string dependencyName)
+		{
+			var match = _OPTIONAL_TAG_REGEX.Match(dependencyName);  //Captures the name itself and a possible optional tag in the form of [optional]
+			if (match.Success)
+			{
+				dependencyName = match.Groups[1].Value;
+				return true;
+			}
+			else
+				return false;
+		}
 
-        public static void HandleOptionalDependencies(IEnumerable<IModuleInfo> modules)
-        {
-            var knownModules = modules.Select(x => x.ModuleName).ToList();
-            foreach (var module in modules)
-                ReplaceOptionalDependencies(module, knownModules);
-        }
+		public static void HandleOptionalDependencies(IEnumerable<IModuleInfo> modules)
+		{
+			var knownModules = modules.Select(x => x.ModuleName).ToList();
+			foreach (var module in modules)
+			{
+				ReplaceOptionalDependencies(module, knownModules);
+			}
+		}
 
-        private static void ReplaceOptionalDependencies(IModuleInfo module, ICollection<string> knownModules)
-        {
-            module.DependsOn.ForEachIndexed((dependency, i) =>
-            {
-                string current = dependency;
-                if (IsOptional(ref current))
-                {
-                    module.DependsOn.Remove(dependency); //We must remove it in any case at it contains the tag
-                    if (knownModules.Contains(current)) //We only add it back (without the tag) when the dependency is actually solvable.
-                        module.DependsOn[i] = current;
-                }
-            });
-        }
-    }
+		private static void ReplaceOptionalDependencies(IModuleInfo module, ICollection<string> knownModules)
+		{
+			module.DependsOn.ForEachIndexed((dependency, i) =>
+			{
+				string current = dependency;
+				if (IsOptional(ref current))
+				{
+					module.DependsOn.Remove(dependency); //We must remove it in any case at it contains the tag
+					if (knownModules.Contains(current)) //We only add it back (without the tag) when the dependency is actually solvable.
+						module.DependsOn[i] = current;
+				}
+			});
+		}
+	}
 }
